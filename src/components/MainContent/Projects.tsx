f0dbc01658c1038b8c6ec6ac6bfe8df377e6d2f6
UnkowwnPhysics <davidferreiragithub@gmail.com>
2025-09-04 16:07:26 +0100

    Update Projects.tsx

--- src/components/MainContent/Projects.tsx
+++ src/components/MainContent/Projects.tsx
@@ -2,9 +2,10 @@ const Projects = () => {
   return (
     <div>
       <h1>Projects</h1>
-      <p>Showcase your best projects here.</p>
+      <p></p>
     </div>
   );
 };
 
 export default Projects;
+
